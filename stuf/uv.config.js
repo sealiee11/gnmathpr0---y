ac023d0f9d2e8f1d3ba945bd7d7245e7db761da6
sealiee1 <159606347+sealiee11@users.noreply.github.com>
2026-03-31 16:39:52 -0700

    Add files via upload

---
+++ stuf/uv.config.js
@@ -0,0 +1,11 @@
+/*global Ultraviolet*/
+self.__uv$config = {
+    prefix: "stuf/service/",
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: "/gh/sealiee11/gnmathpr0-y@main/stuf/uv.handler.js",
+    client: "/gh/sealiee11/gnmathpr0-y@main/stuf/uv.client.js",
+    bundle: "/gh/sealiee11/gnmathpr0-y@main/stuf/uv.bundle.js",
+    config: "/gh/sealiee11/gnmathpr0-y@main/stuf/uv.config.js",
+    sw: "/gh/sealiee11/gnmathpr0-y@main/stuf/uv.sw.js",
+};
